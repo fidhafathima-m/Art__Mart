3a63ab0c75506ab00f10d2fd7a46113e0107c7b3
fidhafathima-m <fidhumusthafa3549@gmail.com>
2025-02-10 16:17:03 +0530

    bug fix

--- config/db.js
+++ config/db.js
@@ -1,19 +1,20 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
 const mongoose = require("mongoose");
-// eslint-disable-next-line no-undef, no-unused-vars
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 
 const dbConnect = async () => {
   try {
-    // eslint-disable-next-line no-undef
-    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("MongoDB URI: ", process.env.MONGODB_URL);
+    // Make sure you use the environment variable MONGODB_URL
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("DB connected successfully");
   } catch (error) {
     console.log("DB connection error: ", error.message);
-    // eslint-disable-next-line no-undef
     process.exit(1);
   }
 };
 
-// eslint-disable-next-line no-undef
 module.exports = dbConnect;
