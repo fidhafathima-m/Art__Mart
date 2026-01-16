e48601cd078ab4299f7d7bb94ef48e628e216979
Fidha Fathima M <fidhumusthafa3549@gmail.com>
2026-01-16 14:19:00 +0530

    testing db atlas connection

--- config/db.js
+++ config/db.js
@@ -6,8 +6,6 @@ const dbConnect = async () => {
   try {
     // Make sure you use the environment variable MONGODB_URL
     await mongoose.connect(process.env.MONGODB_URL);
-    console.log("Connected DB:", mongoose.connection.db.databaseName);
-    console.log("Connected to: ", process.env.MONGODB_URL);
     console.log("DB connected successfully");
   } catch (error) {
     console.log("DB connection error: ", error.message);
